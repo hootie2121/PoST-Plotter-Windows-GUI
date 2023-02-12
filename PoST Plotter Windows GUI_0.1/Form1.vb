e0370bb5467bf89ad00b9b0aee6e8103fecc4019
hootie2121 <hootie2121@outlook.com>
2023-02-12 18:23:33 -0500

    Addressed config problems

--- PoST Plotter Windows GUI_0.1/Form1.vb
+++ PoST Plotter Windows GUI_0.1/Form1.vb
@@ -33,9 +33,111 @@ Public Class Form1
         PopulateCLCombo()
         PopulateNumThreadsCombo()
         PopulateCPortCombo()
-        ReadOrCreateConfigFile()
-        CorrectMissingConfigFileEntries()
+        Dim allValid As Boolean = True
+        For Each program In cliPrograms
+            Dim filePath As String = ""
+            If cliPrograms.TryGetValue(program.Key, filePath) Then
+                If System.IO.File.Exists(filePath) Then
+                    ' The file path is valid, move on to the next program
+                    Continue For
+                End If
+            End If
+
+            ' Check the folder the program is run from for the program
+            Dim currentDirectory As String = System.IO.Directory.GetCurrentDirectory()
+            filePath = System.IO.Path.Combine(currentDirectory, program.Value)
+            If System.IO.File.Exists(filePath) Then
+                ' The file path is valid, update the file path in the dictionary
+                cliPrograms(program.Key) = filePath
+                Continue For
+            End If
+
+            ' If the file path is not found or is invalid, set allValid to False
+            allValid = False
+        Next
+
+        If Not allValid Then
+            Try
+                Dim result As DialogResult = MessageBox.Show("One or more paths to PoST Plotters has not been found or is invalid. Would you like to update the paths now?", "Error", MessageBoxButtons.YesNo)
+                If result = DialogResult.No Then
+                    ' User does not want to update the paths, exit the config process
+                    Return
+                End If
+            Catch ex As Exception
+                ' Handle the exception here
+            End Try
+        End If
+
+        If System.IO.File.Exists(configFilePath) Then
+            ' Read the config file
+            Dim configContent As String = System.IO.File.ReadAllText(configFilePath)
+            ' Parse the config content into a dictionary
+            For Each line As String In configContent.Split(Environment.NewLine)
+                Dim parts As String() = line.Split("=")
+                If parts.Length = 2 Then
+                    cliPrograms(parts(0)) = parts(1)
+                End If
+            Next
+
+            ' Prompt the user to select the correct path for each program
+            For Each program In cliPrograms
+                Dim filePath As String = ""
+                If cliPrograms.TryGetValue(program.Key, filePath) Then
+                    If System.IO.File.Exists(filePath) Then
+                        ' The file path is already set, move on to the next program
+                        Continue For
+                    End If
+                End If
+
+                Dim openFileDialog As New OpenFileDialog With {
+                    .Filter = program.Value & "|" & program.Value,
+                    .Title = "Select " & program.Value
+                }
+                If openFileDialog.ShowDialog() = DialogResult.OK Then
+                    ' Update the file path in the config file
+                    filePath = openFileDialog.FileName
+                    cliPrograms(program.Key) = filePath
+                End If
+            Next
+            ' Write the updated file paths to the config file
+            Dim configBuilder As New StringBuilder()
+            For Each argument In cliPrograms
+                configBuilder.AppendLine(argument.Key & "=" & argument.Value)
+            Next
+            System.IO.File.WriteAllText(configFilePath, configBuilder.ToString())
+        Else
+            ' The config file does not exist
+            ' Prompt the user to select the correct path for each program
+            For Each program In cliPrograms
+                Dim filePath As String = ""
+                ' Check the folder the program is run from for the program
+                Dim currentDirectory As String = System.IO.Directory.GetCurrentDirectory()
+                filePath = System.IO.Path.Combine(currentDirectory, program.Value)
+                If System.IO.File.Exists(filePath) Then
+                    ' The file path is valid, update the file path in the dictionary
+                    cliPrograms(program.Key) = filePath
+                    Continue For
+                End If
+
+                Dim openFileDialog As New OpenFileDialog With {
+                .Filter = program.Value & "|" & program.Value,
+                .Title = "Select " & program.Value
+            }
+                If openFileDialog.ShowDialog() = DialogResult.OK Then
+                    ' Update the file path in the dictionary
+                    filePath = openFileDialog.FileName
+                    cliPrograms(program.Key) = filePath
+                End If
+            Next
+            ' Write the file paths to the new config file
+            Dim configBuilder As New StringBuilder()
+            For Each argument In cliPrograms
+                configBuilder.AppendLine(argument.Key & "=" & argument.Value)
+            Next
+            System.IO.File.WriteAllText(configFilePath, configBuilder.ToString())
+        End If
     End Sub
+
 
     Private Sub SetDebugControlsVisibility(visible As Boolean)
         DebugPM.Visible = visible
@@ -77,139 +179,6 @@ Public Class Form1
 
     Private Sub PopulateCPortCombo()
         CPortCombo.Items.AddRange({"Chia", "MMX", "Other"})
-    End Sub
-
-    Private Sub ReadOrCreateConfigFile()
-        If System.IO.File.Exists(configFilePath) Then
-            ReadConfigFile()
-        Else
-            CreateConfigFile()
-        End If
-    End Sub
-
-    Private Sub ReadConfigFile()
-        ' Read the config file
-        Dim configContent As String = System.IO.File.ReadAllText(configFilePath)
-        ' Parse the config content into a dictionary
-        For Each line As String In configContent.Split(Environment.NewLine)
-            Dim parts As String() = line.Split("=")
-            If parts.Length = 2 Then
-                cliPrograms(parts(0)) = parts(1)
-            End If
-        Next
-        ' Check if the file paths for each program are present and valid
-        For Each program In cliPrograms
-            Dim filePath As String = ""
-            If cliPrograms.TryGetValue(program.Key, filePath) Then
-                If System.IO.File.Exists(filePath) Then
-                    ' The file path is valid, move on to the next program
-                    Continue For
-                End If
-            End If
-
-            ' The file path is not present or invalid
-            ' Prompt the user to select the correct path using the Windows File Explorer dialog box
-            Dim openFileDialog As New OpenFileDialog With {
-            .Filter = program.Value & "|" & program.Value,
-            .Title = "Select " & program.Value
-        }
-            If openFileDialog.ShowDialog() = DialogResult.OK Then
-                ' Update the file path in the config file
-                filePath = openFileDialog.FileName
-                cliPrograms(program.Key) = filePath
-            End If
-        Next
-
-        ' Write the updated file paths to the config file
-        Dim configBuilder As New StringBuilder()
-        For Each argument In cliPrograms
-            configBuilder.AppendLine(argument.Key & "=" & argument.Value)
-        Next
-        System.IO.File.WriteAllText(configFilePath, configBuilder.ToString())
-    End Sub
-
-    Private Sub CreateConfigFile()
-        For Each program In cliPrograms
-            Dim openFileDialog As New OpenFileDialog With {
-                .Filter = program.Value & "|" & program.Value,
-                .Title = "Select " & program.Value
-            }
-            If openFileDialog.ShowDialog() = DialogResult.OK Then
-                ' Update the file path in the dictionary
-                Dim filePath As String = openFileDialog.FileName
-                cliPrograms(program.Key) = filePath
-            End If
-        Next
-        ' Write the file paths to the new config file
-        Dim configBuilder As New StringBuilder()
-        For Each argument In cliPrograms
-            configBuilder.AppendLine(argument.Key & "=" & argument.Value)
-        Next
-        System.IO.File.WriteAllText(configFilePath, configBuilder.ToString())
-    End Sub
-
-    Private Sub CorrectMissingConfigFileEntries()
-        If System.IO.File.Exists(configFilePath) Then
-            ' Read the config file
-            Dim configContent As String = System.IO.File.ReadAllText(configFilePath)
-            ' Parse the config content into a dictionary
-            For Each line As String In configContent.Split(Environment.NewLine)
-                Dim parts As String() = line.Split("=")
-                If parts.Length = 2 Then
-                    cliPrograms(parts(0)) = parts(1)
-                End If
-            Next
-
-            ' Check if the file paths for each program are present and valid
-            For Each program In cliPrograms
-                Dim filePath As String = ""
-                If cliPrograms.TryGetValue(program.Key, filePath) Then
-                    If System.IO.File.Exists(filePath) Then
-                        ' The file path is valid, move on to the next program
-                        Continue For
-                    End If
-                End If
-
-                ' The file path is not present or invalid
-                ' Prompt the user to select the correct path using the Windows File Explorer dialog box
-                Dim openFileDialog As New OpenFileDialog With {
-                    .Filter = program.Value & "|" & program.Value,
-                    .Title = "Select " & program.Value
-                }
-                If openFileDialog.ShowDialog() = DialogResult.OK Then
-                    ' Update the file path in the config file
-                    filePath = openFileDialog.FileName
-                    cliPrograms(program.Key) = filePath
-                End If
-            Next
-
-            ' Write the updated file paths to the config file
-            Dim configBuilder As New StringBuilder()
-            For Each argument In cliPrograms
-                configBuilder.AppendLine(argument.Key & "=" & argument.Value)
-            Next
-            System.IO.File.WriteAllText(configFilePath, configBuilder.ToString())
-        Else
-            ' The config file does not exist
-            ' Create a new config file and prompt the user to select the correct path for each program
-            For Each program In cliPrograms
-                Dim openFileDialog As New OpenFileDialog With {
-                    .Filter = program.Value & "|" & program.Value,
-                    .Title = "Select " & program.Value
-                }
-                If openFileDialog.ShowDialog() = DialogResult.OK Then
-                    ' Update the file path in the dictionary
-                    Dim filePath As String = openFileDialog.FileName
-                    cliPrograms(program.Key) = filePath
-                End If
-            Next
-            ' Write the file paths to the new config file
-            Dim configBuilder As New StringBuilder()
-            For Each argument In cliPrograms
-                configBuilder.AppendLine(argument.Key & "=" & argument.Value)
-            Next
-            System.IO.File.WriteAllText(configFilePath, configBuilder.ToString())
-        End If
     End Sub
 
     Private Sub DebugModeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DebugModeToolStripMenuItem.Click
@@ -851,7 +820,7 @@ Public Class Form1
             For Each arg In commonArguments
                 If arguments.ContainsKey(arg) AndAlso arguments(arg) <> "" Then
                     argumentsString &= " " & arg & " " & arguments(arg)
-            End If
+                End If
             Next
             If arguments.ContainsKey("-g") AndAlso arguments("-g") <> "" Then
                 argumentsString &= " -g " & arguments("-g")
@@ -869,7 +838,7 @@ Public Class Form1
             For Each arg In commonArguments
                 If arguments.ContainsKey(arg) AndAlso arguments(arg) <> "" Then
                     argumentsString &= " " & arg & " " & arguments(arg)
-            End If
+                End If
             Next
             If arguments.ContainsKey("-r") AndAlso arguments("-r") <> "" Then
                 argumentsString &= " -r " & arguments("-r")
@@ -880,13 +849,13 @@ Public Class Form1
             If arguments.ContainsKey("-v") AndAlso arguments("-v") <> "" Then
                 argumentsString &= " -v " & arguments("-v")
             End If
-            End If
+        End If
 
-            Dim processStartInfo As New ProcessStartInfo(plotFilePath, argumentsString)
-            processStartInfo.WorkingDirectory = plotDirectory
-            Dim cmdProcess As New Process()
-            cmdProcess.StartInfo = processStartInfo
-            cmdProcess.Start()
+        Dim processStartInfo As New ProcessStartInfo(plotFilePath, argumentsString)
+        processStartInfo.WorkingDirectory = plotDirectory
+        Dim cmdProcess As New Process()
+        cmdProcess.StartInfo = processStartInfo
+        cmdProcess.Start()
     End Sub
 
 End Class
